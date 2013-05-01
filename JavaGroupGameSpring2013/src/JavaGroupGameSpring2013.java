933db0780e2de90506d6cceb0f11cfe74dcb8fc4
Christopher Roche <christopherroche@stmarksschool.org>
2013-04-30 21:19:09 -0400

    Mr. Roche--Adds the frame to the game

---
+++ JavaGroupGameSpring2013/src/JavaGroupGameSpring2013.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+
+public class JavaGroupGameSpring2013 {
+
+	/**
+	 This is the Git Hub Version of the Java Game
+	 Started by Mr. Roche with Alex Dolce, David Eacho, Ethan Pailes, 
+	 Paul Tempel and Alex Von Campe, Spring term 2013
+	 */
+	
+	//Instance variables
+	static JFrame f;
+	public static void main(String[] args) {
+		
+		//Start of the game, build a frame
+		f = new JFrame("The Game with No Name Right Now");
+		f.setSize(700, 900);
+		
+		
+		
+		
+		f.setVisible(true);
+		
+
+	}
+
+}
